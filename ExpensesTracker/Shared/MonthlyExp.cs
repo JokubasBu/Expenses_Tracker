01e73e7fabbf6107ca2e3b206bf132d0f7bd8e62
JokubasBu <bucelis.jokubas@gmail.com>
2022-10-14 19:34:23 +0300

    Properties for MonthlyExp.cs dates

--- ExpensesTracker/Shared/MonthlyExp.cs
+++ ExpensesTracker/Shared/MonthlyExp.cs
@@ -8,15 +8,47 @@ namespace ExpensesTracker.Shared
 {
     public class MonthlyExp : IComparable
     {
+        private int _month;
+        private int _year;
+        private int _day;
         public int Id { get; set; } // Id is always primary key by default
         public double Money { get; set; }
         public string Comment { get; set; } = string.Empty;
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
-
-        public int Year { get; set; }
-        public int Month { get; set; }
-        public int Day { get; set; }
+        public int Year
+        {
+            get => _year;
+            set
+            {
+                if ((value > 2000) && ( value < 2024))
+                {
+                    _year = value;
+                }
+            }
+        }
+        public int Month
+        {
+            get => _month;
+            set
+            {
+                if ((value > 0) && (value < 13))
+                {
+                    _month = value;
+                }
+            }
+        }
+        public int Day
+        {
+            get => _day;
+            set
+            {
+                if ((value > 0) && (value< 32))
+                {
+                    _day = value;
+                }
+}
+        }
 
         public int CompareTo(object? obj)
         {
