185b14feda411d13b23af4bab81e5580ba39c4dc
JokubasBu <bucelis.jokubas@gmail.com>
2022-10-18 16:48:46 +0300

    Conflict Resolved

--- ExpensesTracker/Server/Controllers/ExpensesController.cs
+++ ExpensesTracker/Server/Controllers/ExpensesController.cs
@@ -126,13 +126,13 @@ namespace ExpensesTracker.Server.Controllers
             return await context.AllExpenses.Include(e => e.Category).ToListAsync();
         }
 
-        async Task<List<MonthlyExp>> GetFilteredExpenses()
+        async Task<List<Expense>> GetFilteredExpenses()
         {      
-            var expenses = await context.MonthlyExps.Include(e => e.Category).ToListAsync();
+            var expenses = await context.AllExpenses.Include(e => e.Category).ToListAsync();
 
-            currentExpenses = expenses.PickCategory(id: _categoryId);
-            currentExpenses = currentExpenses.PickMonth(monthNr: _month);
-            currentExpenses = currentExpenses.PickYear(year: _year);
+            currentExpenses = expenses.FilterBy(id: _categoryId);
+            currentExpenses = currentExpenses.FilterBy(month: _month);
+            currentExpenses = currentExpenses.FilterBy(year: _year);
 
             currentExpenses.Sort(); //ascending
             if (currentCount % 2 == 0)
