c962ef5182fe6d6a5a4a237c32f4e7f663be99d3
Linas490 <113028160+Linas490@users.noreply.github.com>
2022-11-21 23:34:43 +0200

    User class added

---
+++ ExpensesTracker/Shared/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTracker.Shared.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string nickname { get; set; }
+        public string birth { get; set; }
+        public double balance { get; set; }
+        public string email { get; set; }
+        private static int idQueue = 0;
+
+
+    }
+}
