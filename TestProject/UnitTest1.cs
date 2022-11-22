fcbab4734d9e89da59059359ed0ea8bef26aa681
Deniza <elada.baltika@gmail.com>
2022-11-22 19:51:07 +0200

    clean up, in progress

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -36,15 +36,6 @@ namespace TestProject
         public void Adding()
         {
             ExpensesService a = new ExpensesService(http, navigationManager);
-            //Expense b = new Expense {
-            //    Id = 400,
-            //    Money = 500,
-            //    Comment = "hi",
-            //    Year = 2022,
-            //    Month = 10,
-            //    Day = 15,
-            //    CategoryId = 2
-            //};
 
             using (var context = new DataContext(options))
             {
@@ -60,24 +51,62 @@ namespace TestProject
             {
                 Assert.NotNull(a.GetSingleExpense(400));
                 Assert.False(b.Equals(a.GetSingleExpense(12)));
+                
+                //var actionResult = await a.GetSingleExpense(400);
+                //Assert.True(b.Equals(actionResult));
+                //Assert.Equal(b, (a.GetSingleExpense(400).Result as CreatedAtActionResult).Value);
+                //Assert.Equal(b, (a.GetSingleExpense(400).Result));
             }
         }
+
+        //[Fact]
+        //public void GetExp()
+        //{
+        //    ExpensesService a = new ExpensesService(http, navigationManager);
+        //    Expense c = new Expense
+        //    {
+        //        Id = 2,
+        //        Money = 50,
+        //        Comment = "hello",
+        //        Year = 2021,
+        //        Month = 11,
+        //        Day = 5,
+        //        CategoryId = 3
+        //    };
+        //    Expense d = new Expense
+        //    {
+        //        Id = 5,
+        //        Money = 10,
+        //        Comment = "hey",
+        //        Year = 2020,
+        //        Month = 1,
+        //        Day = 8,
+        //        CategoryId = 1
+        //    };
+
+        //    using (var context = new DataContext(options))
+        //    {
+        //        context.Database.EnsureCreated();
+        //    }
+
+        //    using (var context = new DataContext(options))
+        //    {
+        //        //context.AllExpenses.Add(b);
+        //        context.AllExpenses.Add(c);
+        //        context.AllExpenses.Add(d);
+        //        context.SaveChanges();
+        //    }
+        //    using (var context = new DataContext(options))
+        //    {
+        //        var result = a.GetExpenses();
+        //        //Assert.Contains(result, (IEnumerable<Task>)c); // casting issue
+        //    }
+        //}
 
         [Fact]
         public void Deleting()
         {
             ExpensesService a = new ExpensesService(http, navigationManager);
-            //Expense b = new Expense
-            //{
-            //    Id = 400,
-            //    Money = 500,
-            //    Comment = "hi",
-            //    Year = 2022,
-            //    Month = 10,
-            //    Day = 15,
-            //    CategoryId = 2
-            //};
-
             using (var context = new DataContext(options))
             {
                 context.Database.EnsureCreated();
