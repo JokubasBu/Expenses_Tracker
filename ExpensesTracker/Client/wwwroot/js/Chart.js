6a7d00ff064137703f114c41c28b410164fd09b1
JokubasBu <bucelis.jokubas@gmail.com>
2022-12-14 13:58:36 +0200

    Chart update

--- ExpensesTracker/Client/wwwroot/js/Chart.js
+++ ExpensesTracker/Client/wwwroot/js/Chart.js
@@ -1,32 +1,29 @@
 ﻿function GeneratePieChart(Expenses)
 {
-    
-        // Themes begin
     am4core.useTheme(am4themes_animated);
-        // Themes end
 
-        // Create chart instance
-        var chart = am4core.create("chartdiv", am4charts.PieChart);
+    var chart = am4core.create("chartdiv", am4charts.PieChart);
 
-        // Add data
     chart.data = Expenses;
 
-        // Add and configure Series
-        var pieSeries = chart.series.push(new am4charts.PieSeries());
+    var pieSeries = chart.series.push(new am4charts.PieSeries());
     pieSeries.dataFields.value = "totalExpenses";
-        pieSeries.dataFields.category = "category";
-        pieSeries.slices.template.stroke = am4core.color("#fff");
-        pieSeries.slices.template.strokeOpacity = 1;
+    pieSeries.dataFields.category = "category";
+    pieSeries.slices.template.stroke = am4core.color("#fff");
+    pieSeries.slices.template.strokeOpacity = 1;
 
-        // This creates initial animation
-        pieSeries.hiddenState.properties.opacity = 1;
-        pieSeries.hiddenState.properties.endAngle = -90;
-        pieSeries.hiddenState.properties.startAngle = -90;
-
-    chart.hiddenState.properties.radius = am4core.percent(0);
-
+    // This creates initial animation
+    pieSeries.hiddenState.properties.opacity = 1;
+    pieSeries.hiddenState.properties.endAngle = -90;
+    pieSeries.hiddenState.properties.startAngle = -90;
+    pieSeries.legendSettings.labelText = "{category}: {value}€"
+    pieSeries.legendSettings.valueText = " ";
     
-
-
-} // end am4core.ready()
+    //Legend seetings
+    chart.hiddenState.properties.radius = am4core.percent(0);
+    chart.legend = new am4charts.Legend();
+    pieSeries.labels.template.disabled = true;
+    pieSeries.ticks.template.disabled = true;
+    
+} 
 
